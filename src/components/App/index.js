c4ff354e33ccdac13a7d3bd638d3c4cf0be60f8a
yuchen <yl6448@nyu.edu>
2020-08-16 19:46:31 -0700

    Add location state

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+const DEFAULT_LOCATION = {
+  latitude: 40.7295,
+  longitude: 73.9965
+}
 
 function App() {
+
+  const [location, setLocation] = useState(DEFAULT_LOCATION);
+
+  useEffect(() => {
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        setLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        });
+
+      });
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
