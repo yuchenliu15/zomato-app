388a64996c129bbd96082e86386800ff13e50e6d
yuchen <yl6448@nyu.edu>
2020-08-16 20:15:39 -0700

    Remove location from state and add data

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,24 +1,41 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const DEFAULT_LOCATION = {
   latitude: 40.7295,
   longitude: 73.9965
 }
+// for easy demo, I'll leave the API key exposed
+const ZOMATO = '058759b89bd02ab3ef6c3f62c57e96cd'
 
 function App() {
 
-  const [location, setLocation] = useState(DEFAULT_LOCATION);
+  const [data, setData] = useState({});
+
+  const fetchData = async location => {
+    const data = await axios.get(
+        `https://developers.zomato.com/api/v2.1/geocode?
+          lat=${location.latitude}&lon=${location.longitude}`,
+        {
+          headers: {'user-key': ZOMATO}
+        }
+      );
+    setData(data);
+  }
 
   useEffect(() => {
     if(navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position => {
-        setLocation({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude
-        });
-
-      });
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          fetchData({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          })
+        },
+        (error) => fetchData(DEFAULT_LOCATION)
+      );
     }
+
   }, []);
 
   return (
